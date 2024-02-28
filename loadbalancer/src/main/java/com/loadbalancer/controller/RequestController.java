d860bea05bd1233a92368c3601bd279431b1a5c8
Pradip Mudi <pradipmudi24@gmail.com>
2024-02-28 17:49:32 +0530

    added exception handling and little bit of code cleanup

--- loadbalancer/src/main/java/com/loadbalancer/controller/RequestController.java
+++ loadbalancer/src/main/java/com/loadbalancer/controller/RequestController.java
@@ -3,15 +3,8 @@ package com.loadbalancer.controller;
 import com.loadbalancer.service.LoadBalancerService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.HandlerMapping;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.util.Map;
 
 @RestController
 public class RequestController {
@@ -26,27 +19,9 @@ public class RequestController {
     @RequestMapping(value = "/**", method = {RequestMethod.GET, RequestMethod.PUT, RequestMethod.POST, RequestMethod.PATCH, RequestMethod.DELETE})
     public ResponseEntity<String> routeRequest(HttpServletRequest request,
                                                @RequestBody(required = false) String requestBody) {
-        String method = request.getMethod();
 
-        // Extract path and query parameters separately
-        String requestUri = request.getRequestURI();
-        String queryString = request.getQueryString();
-
-        // Construct the URL with only path parameters
-        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromPath(requestUri);
-
-        // Append query parameters if they exist
-        if (queryString != null) {
-            uriBuilder.query(queryString);
-        }
-
-        String fullUrl = uriBuilder.toUriString();
-
-        // Construct HttpEntity if requestBody is not null
-        HttpEntity<String> requestEntity = requestBody != null ? new HttpEntity<>(requestBody) : null;
-
-        // Call the rerouteRequest method from LoadBalancerService to reroute the request
-        return loadBalancerService.rerouteRequest(HttpMethod.valueOf(method), fullUrl, requestEntity);
+        // Call the forwardRequest method from LoadBalancerService to reroute the request
+        return loadBalancerService.forwardRequest(request, requestBody);
     }
 
 
--- loadbalancer/src/main/java/com/loadbalancer/service/LoadBalancerService.java
+++ loadbalancer/src/main/java/com/loadbalancer/service/LoadBalancerService.java
@@ -7,14 +7,18 @@ import com.loadbalancer.algorithms.WeightedRoundRobinLoadBalancer;
 import com.loadbalancer.config.LoadBalancerConfiguration;
 import com.loadbalancer.config.ServerConfig;
 import com.loadbalancer.constant.LoadBalancingAlgorithm;
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -64,9 +68,9 @@ public class LoadBalancerService {
         LoadBalancer loadBalancer = loadTheLoadBalancingAlgorithmByConfig();
         ServerConfig server = loadBalancer.getNextEligibleServer();
 
-        if (loadBalancer instanceof LeastConnectionsLoadBalancer) {
+        if (loadBalancer instanceof LeastConnectionsLoadBalancer leastConnectionsLoadBalancer) {
             logger.debug("Incrementing connections for server: {}", server);
-            ((LeastConnectionsLoadBalancer) loadBalancer).incrementConnections(server);
+            leastConnectionsLoadBalancer.incrementConnections(server);
         }
 
         // Construct the URL for the selected server
@@ -77,11 +81,45 @@ public class LoadBalancerService {
         // Forward the request to the selected server
         ResponseEntity<String> responseEntity = restTemplate.exchange(serverUrl, method, requestEntity, String.class);
 
-        if (loadBalancer instanceof LeastConnectionsLoadBalancer) {
+        if (loadBalancer instanceof LeastConnectionsLoadBalancer leastConnectionsLoadBalancer) {
             logger.debug("Decrementing connections for server: {}", server);
-            ((LeastConnectionsLoadBalancer) loadBalancer).decrementConnections(server);
+            leastConnectionsLoadBalancer.decrementConnections(server);
         }
 
         return responseEntity;
+    }
+
+    public ResponseEntity<String> forwardRequest(HttpServletRequest request, String requestBody) {
+        try {
+            String method = request.getMethod();
+
+            // Extract path and query parameters separately
+            String requestUri = request.getRequestURI();
+            String queryString = request.getQueryString();
+
+            // Construct the URL with only path parameters
+            UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromPath(requestUri);
+
+            // Append query parameters if they exist
+            if (queryString != null) {
+                uriBuilder.query(queryString);
+            }
+
+            String fullUrl = uriBuilder.toUriString();
+
+            // Construct HttpEntity if requestBody is not null
+            HttpEntity<String> requestEntity = requestBody != null ? new HttpEntity<>(requestBody) : null;
+
+            // Call rerouteRequest method and return the response
+            return rerouteRequest(HttpMethod.valueOf(method), fullUrl, requestEntity);
+        } catch (HttpClientErrorException e) {
+            // Handle HttpClientErrorException
+            // For example, return ResponseEntity with the appropriate status code and error message
+            return ResponseEntity.status(e.getStatusCode()).body("{ HTTP Method: "+request.getMethod()+", HTTP Status - "+e.getMessage()+" } ");
+        } catch (Exception e) {
+            // Handle other exceptions
+            // For example, return ResponseEntity with internal server error status code and a generic error message
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error");
+        }
     }
 }
